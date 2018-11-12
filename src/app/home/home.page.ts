4ea78efca71a0d477da504f1eb3a1bcfba7c6888
alex_laptop <abcbogdan11@gmail.com>
2018-11-12 18:45:26 +0000

    sound start

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+// import { ToneJs } from 'tone';
+import * as tone from 'tone';
+
 
 @Component({
   selector: 'app-home',
@@ -24,304 +27,57 @@ export class HomePage {
     constructor() {
         // this.buttons = ['1', '2/abc', '3/def', '4/ghi', '5/jkl', '6/mno', '7/pqrs', '8/tuv', '9/wxyz', '*/+', '0/_', '#/UP'];
     }
-
-
     buttonClicked(num) {
-        const context = new AudioContext();
-        const o = context.createOscillator();
-        o.type = 'sine';
-        o.connect(context.destination);
-        o.start();
-
-        const interval = window.setInterval(() => {
-            o.stop();
-            window.clearInterval(interval);
-        }, 3000);
+        const test = new tone.Synth().toMaster();
+        // // const boppen = new ToneJs.Synth().toMaster();
+        // // // var synth = new Tone.Synth().toMaster();
+        test.triggerAttackRelease('C4', '8n');
+        // const context = new AudioContext();
+        // const o = context.createOscillator();
+        // o.type = 'sine';
+        // o.connect(context.destination);
+        // o.start();
+        //
+        // const interval = window.setInterval(() => {
+        //     o.stop();
+        //     window.clearInterval(interval);
+        // }, 3000);
 
         if (1 === num) {
-            const newDate = new Date();
-            if (newDate.getTime() - this.lastTapTime < this.delay) {
-                this.counter++;
-                this.lastTapTime = new Date().getTime();
-            } else {
-                this.lastTapTime = new Date().getTime();
-                this.counter = 1;
-            }
-            const interval = window.setInterval(() => {
-                if (this.counter === 1) {
-                    this.counter = 0;
-                    this.myVar = this.myVar + 'a';
-                    this.timeoutDuration = this.delay;
-                } else if (this.counter === 2 ) {
-                    this.counter = 0 ;
-                    this.myStr = 'b';
-                    this.myVar = this.myVar + 'b';
-                    this.timeoutDuration = this.timeoutDuration + this.delay;
-                } else if (this.counter === 3) {
-                    this.timeoutDuration = this.timeoutDuration + this.delay;
-                    this.counter = 0;
-                    this.myVar = this.myVar + 'c';
-                } else{
-                    this.delay = 400;
-                    this.counter = 0;
-                }
-                window.clearInterval(interval);
-            }, this.timeoutDuration);
+            this.myVar = this.myVar + 'c';
         }
         if (2 === num) {
-            const newDate = new Date();
-            if (newDate.getTime() - this.lastTapTime < this.delay) {
-                this.counter++;
-                this.lastTapTime = new Date().getTime();
-            } else {
-                this.lastTapTime = new Date().getTime();
-                this.counter = 1;
-            }
-            const interval = window.setInterval(() => {
-                if (this.counter === 1) {
-                    this.counter = 0;
-                    this.myVar = this.myVar + 'a';
-                    this.timeoutDuration = this.delay;
-                } else if (this.counter === 2 ) {
-                    this.counter = 0 ;
-                    this.myVar = this.myVar + 'b';
-                    this.timeoutDuration = this.timeoutDuration + this.delay;
-                } else if (this.counter === 3) {
-                    this.timeoutDuration = this.timeoutDuration + this.delay;
-                    this.counter = 0;
-                    this.myVar = this.myVar + 'c';
-                } else{
-                    this.delay = 400;
-                    this.counter = 0;
-                }
-                window.clearInterval(interval);
-            }, this.timeoutDuration);
+            this.myVar = this.myVar + 'd';
         }
         if (3 === num) {
-            const newDate = new Date();
-            if (newDate.getTime() - this.lastTapTime < this.delay) {
-                this.counter++;
-                this.lastTapTime = new Date().getTime();
-            } else {
-                this.lastTapTime = new Date().getTime();
-                this.counter = 1;
-            }
-            const interval = window.setInterval(() => {
-                if (this.counter === 1) {
-                    this.counter = 0;
-                    this.myVar = this.myVar + 'd';
-                    this.timeoutDuration = this.delay;
-                } else if (this.counter === 2 ) {
-                    this.counter = 0 ;
-                    this.myVar = this.myVar + 'e';
-                    this.timeoutDuration = this.timeoutDuration + this.delay;
-                } else if (this.counter === 3) {
-                    this.timeoutDuration = this.timeoutDuration + this.delay;
-                    this.counter = 0;
-                    this.myVar = this.myVar + 'f';
-                } else{
-                    this.delay = 400;
-                    this.counter = 0;
-                }
-                window.clearInterval(interval);
-            }, this.timeoutDuration);
+            this.myVar = this.myVar + 'e';
         }
         if (4 === num) {
-            const newDate = new Date();
-            if (newDate.getTime() - this.lastTapTime < this.delay) {
-                this.counter++;
-                this.lastTapTime = new Date().getTime();
-            } else {
-                this.lastTapTime = new Date().getTime();
-                this.counter = 1;
-            }
-            const interval = window.setInterval(() => {
-                if (this.counter === 1) {
-                    this.counter = 0;
-                    this.myVar = this.myVar + 'g';
-                    this.timeoutDuration = this.delay;
-                } else if (this.counter === 2 ) {
-                    this.counter = 0 ;
-                    this.myVar = this.myVar + 'h';
-                    this.timeoutDuration = this.timeoutDuration + this.delay;
-                } else if (this.counter === 3) {
-                    this.timeoutDuration = this.timeoutDuration + this.delay;
-                    this.counter = 0;
-                    this.myVar = this.myVar + 'i';
-                } else{
-                    this.delay = 400;
-                    this.counter = 0;
-                }
-                window.clearInterval(interval);
-            }, this.timeoutDuration);
+            this.myVar = this.myVar + 'f';
         }
         if (5 === num) {
-            const newDate = new Date();
-            if (newDate.getTime() - this.lastTapTime < this.delay) {
-                this.counter++;
-                this.lastTapTime = new Date().getTime();
-            } else {
-                this.lastTapTime = new Date().getTime();
-                this.counter = 1;
-            }
-            const interval = window.setInterval(() => {
-                if (this.counter === 1) {
-                    this.counter = 0;
-                    this.myVar = this.myVar + 'j';
-                    this.timeoutDuration = this.delay;
-                } else if (this.counter === 2 ) {
-                    this.counter = 0 ;
-                    this.myVar = this.myVar + 'k';
-                    this.timeoutDuration = this.timeoutDuration + this.delay;
-                } else if (this.counter === 3) {
-                    this.timeoutDuration = this.timeoutDuration + this.delay;
-                    this.counter = 0;
-                    this.myVar = this.myVar + 'l';
-                } else{
-                    this.delay = 400;
-                    this.counter = 0;
-                }
-                window.clearInterval(interval);
-            }, this.timeoutDuration);
+            this.myVar = this.myVar + 'g';
         }
         if (6 === num) {
-            const newDate = new Date();
-            if (newDate.getTime() - this.lastTapTime < this.delay) {
-                this.counter++;
-                this.lastTapTime = new Date().getTime();
-            } else {
-                this.lastTapTime = new Date().getTime();
-                this.counter = 1;
-            }
-            const interval = window.setInterval(() => {
-                if (this.counter === 1) {
-                    this.counter = 0;
-                    this.myVar = this.myVar + 'm';
-                    this.timeoutDuration = this.delay;
-                } else if (this.counter === 2 ) {
-                    this.counter = 0 ;
-                    this.myVar = this.myVar + 'n';
-                    this.timeoutDuration = this.timeoutDuration + this.delay;
-                } else if (this.counter === 3) {
-                    this.timeoutDuration = this.timeoutDuration + this.delay;
-                    this.counter = 0;
-                    this.myVar = this.myVar + 'o';
-                } else{
-                    this.delay = 400;
-                    this.counter = 0;
-                }
-                window.clearInterval(interval);
-            }, this.timeoutDuration);
+            this.myVar = this.myVar + 'a';
         }
         if (7 === num) {
-            const newDate = new Date();
-            if (newDate.getTime() - this.lastTapTime < this.delay) {
-                this.counter++;
-                this.lastTapTime = new Date().getTime();
-            } else {
-                this.lastTapTime = new Date().getTime();
-                this.counter = 1;
-            }
-            const interval = window.setInterval(() => {
-                if (this.counter === 1) {
-                    this.counter = 0;
-                    this.myVar = this.myVar + 'p';
-                    this.timeoutDuration = this.delay;
-                } else if (this.counter === 2 ) {
-                    this.counter = 0 ;
-                    this.myVar = this.myVar + 'q';
-                    this.timeoutDuration = this.timeoutDuration + this.delay;
-                } else if (this.counter === 3) {
-                    this.timeoutDuration = this.timeoutDuration + this.delay;
-                    this.counter = 0;
-                    this.myVar = this.myVar + 'r';
-                } else if (this.counter === 4) {
-                        this.timeoutDuration = this.timeoutDuration + this.delay;
-                        this.counter = 0;
-                        this.myVar = this.myVar + 's';
-                } else{
-                    this.delay = 400;
-                    this.counter = 0;
-                }
-                window.clearInterval(interval);
-            }, this.timeoutDuration);
+            this.myVar = this.myVar + 'b';
         }
         if (8 === num) {
-            const newDate = new Date();
-            if (newDate.getTime() - this.lastTapTime < this.delay) {
-                this.counter++;
-                this.lastTapTime = new Date().getTime();
-            } else {
-                this.lastTapTime = new Date().getTime();
-                this.counter = 1;
-            }
-            const interval = window.setInterval(() => {
-                if (this.counter === 1) {
-                    this.counter = 0;
-                    this.myVar = this.myVar + 't';
-                    this.timeoutDuration = this.delay;
-                } else if (this.counter === 2 ) {
-                    this.counter = 0 ;
-                    this.myVar = this.myVar + 'u';
-                    this.timeoutDuration = this.timeoutDuration + this.delay;
-                } else if (this.counter === 3) {
-                    this.timeoutDuration = this.timeoutDuration + this.delay;
-                    this.counter = 0;
-                    this.myVar = this.myVar + 'v';
-                } else{
-                    this.delay = 400;
-                    this.counter = 0;
-                }
-                window.clearInterval(interval);
-            }, this.timeoutDuration);
+            this.myVar = this.myVar + '-';
         }
         if (9 === num) {
-            const newDate = new Date();
-            if (newDate.getTime() - this.lastTapTime < this.delay) {
-                this.counter++;
-                this.lastTapTime = new Date().getTime();
-            } else {
-                this.lastTapTime = new Date().getTime();
-                this.counter = 1;
-            }
-            const interval = window.setInterval(() => {
-                if (this.counter === 1) {
-                    this.counter = 0;
-                    this.myVar = this.myVar + 'w';
-                    this.timeoutDuration = this.delay;
-                } else if (this.counter === 2 ) {
-                    this.counter = 0 ;
-                    this.myVar = this.myVar + 'x';
-                    this.timeoutDuration = this.timeoutDuration + this.delay;
-                } else if (this.counter === 3) {
-                    this.timeoutDuration = this.timeoutDuration + this.delay;
-                    this.counter = 0;
-                    this.myVar = this.myVar + 'y';
-                } else if (this.counter === 4) {
-                    this.timeoutDuration = this.timeoutDuration + this.delay;
-                    this.counter = 0;
-                    this.myVar = this.myVar + 'z';
-                } else{
-                    this.delay = 400;
-                    this.counter = 0;
-                }
-                window.clearInterval(interval);
-            }, this.timeoutDuration);
+            this.myVar = this.myVar + '+';
         }
         if (10 === num) {
-            const newDate = new Date();
-            this.myVar = this.myVar + '10';
-            console.log(newDate.getTime());
+            this.myVar = this.myVar + 'oct';
         }
         if (11 === num) {
-            const newDate = new Date();
-            this.myVar = this.myVar + '11';
-            console.log(newDate.getTime());
+            this.myVar = this.myVar + 'rest';
         }
         if (12 === num) {
-            const newDate = new Date();
-            this.myVar = this.myVar + '12';
-            console.log(newDate.getTime());
+            this.myVar = this.myVar + '#';
         }
     }
 
