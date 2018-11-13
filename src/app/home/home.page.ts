ab68d30dd2e43b6199d67acc26ac1975cc37176a
alex <abcbogdan11@gmail.com>
2018-11-13 00:18:13 +0000

    branch

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -115,6 +115,38 @@ export class HomePage {
     reset() {
         this.myVar = '';
     }
+
+    deleteLast() {
+        const myIndex = this.myVar.substr(0, this.myVar.length - 1).lastIndexOf(' ');
+        if (this.myVar.length <= 6) {
+            this.myVar = '';
+        } else {
+                this.myVar = this.myVar.substr(0, myIndex) + ' ';
+            }
+        }
+    play() {
+        const playThis = new tone.Synth().toMaster();
+        // // var synth = new Tone.FMSynth().toMaster()
+        //
+        // console.log('play');
+        // const myArray = this.myVar.substring(0, this.myVar.length - 1).split(' ');
+        // for ( let i = 0; i < myArray.length; i++){
+        //     console.log(myArray[i]);
+        //     const match = /[a-zA-Z]/.exec(myArray[i]);
+        //     if (match) {
+        //         const duration = parseInt(myArray[i].substr(0, match.index));
+        //         // this.myVar = this.myVar.substr(0, myIndex) + ' ' + duration + lastInstructions.substr(match.index) + ' ';
+        //         // playThis.triggerAttackRelease(myArray[i].substring(match.index, match.index + 1) + '#1', duration + 'n', tone.Time('4n') + tone.Time('8n'));
+        //     }
+        // }
+
+        const part = new tone.Part(function(time, value){
+            playThis.triggerAttackRelease(value.note, '8n', time, value.velocity);
+        }, [{'time' : 0, 'note' : 'C3', 'velocity': 0.9},
+            {'time' : '0:2', 'note' : 'C4', 'velocity': 0.5}
+        ]).start(0);
+
+    }
 }
 
 
