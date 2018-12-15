f634c994f67e9551ee102afd23888c88dfc75e48
Cian <cian.mchale.2016@mumail.ie>
2018-12-15 23:09:35 +0000

    Updated UI

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,6 +3,8 @@ import * as tone from 'tone';
 import { Options } from 'ng5-slider';
 import { FormControl } from '@angular/forms';
 import { TestBed } from '@angular/core/testing';
+//import {bufferToWav} from 'audiobuffer-to-wav';
+
 // @ViewChild('play') play;
 
 
@@ -19,6 +21,9 @@ export class HomePage {
         floor: 0,
         ceil: 400
     };
+    public effectName: any;
+    public test123;
+    public audioContext = new AudioContext();
     public bitcrusher;
     public duosynth;
     public distortion;
@@ -123,74 +128,9 @@ export class HomePage {
                 }
             }
 
-        ]
-        // this.songs = [
-        //     paradise: {
-        //         notes: '8d1 4c1 4#c1 8d1 4c1 4#c1 4d1 4c1 4d1 4c1 4d1 8f1 8d1 4c1 4#c1 8d1 4c1 4#c1 4d1 4c1 4d1 4c1 4d1 8f1',
-        //         tempo: 225
-        //     },
-        //     without: {
-        //         notes: '8d2 8#a2 8a2 8g2 4d2 4- 8d2 8c2 8d2 8f2 8d2 8- 4- 8d2 8c2 8d2 8c2 8#a1 8- 4- 8#a1 8a1 8f1 4g1 4- 8- 8d2 8#a2 8a2 8g2 8d2 8- 4- 8d2 8c2 8d2 8f2 4d2 4- 8d2 8c2 8d2 8c2 8#a1 8- 4- 8#a1 8a1 8f1 8g1',
-        //         tempo: 150
-        //     },
-        //     files: {
-        //         notes: '4e1 4b1 4a1 4b1 4d2 2b1 1- 4e1 4b1 4a1 4b1 4e2 2b1 1- 4g2 4#f2 4e2 4d2 4e2 2b1 1- 4g2 4#f2 4e2 4d2 4#f2 2b1 1- 4e1 4b1 4a1 4b1 4d2 2b1 1- 4e1 4b1 4a1 4b1 4e2 2b1 1- 4e2 2b1',
-        //         tempo: 125
-        //     },
-        //     barbie: {
-        //         notes: '8#g2 8e2 8#g2 8#c3 4a2 4- 8#f2 8#d2 8#f2 8b2 4#g2 8#f2 8e2 4- 8e2 8#c2 4#f2 4#c2 4- 8#f2 8e2 4#g2 4#f2',
-        //         tempo: 190
-        //     },
+        ];
 
-        //     beat: {
-        //         notes: '8e1 4g1 4b1 4g2 4e2 4- 4e2 8#f2 4e2 4d2 4- 8d2 4- 8e1 4g1 4b1 4g2 4e2 4- 4e2 8#f2 4e2 4d2',
-        //         tempo: 225
-        //     },
-        //     match: {
-        //         notes: '8c1 8f1 8a1 8.c2 16a1 8a1 8a1 8a1 4a1 8#a1 8.c2 16a1 8g1 8a1 8#a1 8c1 8e1 8g1 8.#a1 16g1 8g1 8g1 8g1 4g1 8a1 8.#a1 16g1 8f1 8g1 8a1 8c1 8f1 8a1 8.c2 16a1 8a1 8a1 8a1 4a1 8#a1 8.c2 16a1 8#a1 8c2 4d2 8d2 8e2 8f2 16f2 8e2 16e2 8d2 8f2 8c2 8c2 8d2 8c2 16#a1 8a1 16a1 8g1 4f1',
-        //         tempo: 130
-        //     },
-        //     smooth: {
-        //         notes: ' 8a1 16a1 16a1 16g1 16a1 8b1 8b1 8- 16a1 16b1 8c2 8c2 8- 16b1 16c2 8b1 4g1 8a1 8- 8a1 16a1 16a1 16g1 16a1 8b1 8b1 8- 16a1 16b1 8c2 8c2 8- 16b1 16c2 8b1 4g1',
-        //         tempo: 125
-        //     },
-        //     mission: {
-        //         notes: '16g2 8- 16g2 8- 16f2 16- 16#f2 16- 16g2 8- 16g2 8- 16#a2 16- 16c3 16- 16g2 8- 16g2 8- 16f2 16- 16#f2 16- 16g2 8- 16g2 8- 16#a2 16- 16c3 16- 16#a2 16g2 2d2 32- 16#a2 16g2 2#c2 32- 16#a2 16g2 2c2 16- 16#a1 16c2',
-        //         tempo: 250
-        //     },
-        //     jamboo: {
-        //         notes: '8g1 8c2 8g2 4g1 8c2 8g2 4#d2 8d2 8c2 8#g1 8c2 8#d2 4#g1 8#a1 8f2 4#d2 8d2 8c2 8g1 8c2 8g2 4g1 8c2 8g2 4#d2 8d2 8c2 8c2 8d2 8#d2 4c2 8f2',
-        //         tempo: 225
-        //     },
-        //     adams: {
-        //         notes: '8#f1 16#f1 16#f1 16#f1 8b1 32#d2 8b1 32#g1 8e1 8#c2 32a1 8#a1 32#c2 8#a1 32#f1 8#d1 8b1 32#f1 8b1 32#d2 8b1 32#g1 8e1 8#c2 32b1 8#a1 32#f1 8#g1 32#a1 4b1 32#f1 8b1 32#d2 8b1 32#g1 8e1 8#c2 32a1 8#a1 32#c2 8#a1 32#f1 8#d1 8b1 32#f1 8b1 32#d2 8b1 32#g1 8e1',
-        //         tempo: 70
-        //     },
-        //     cucaracha: {
-        //         notes: '4c1 4c1 4c1 8f1 8a1 4- 4c1 4c1 4c1 8f1 8a1 4- 4f1 4f1 4e1 4e1 4d1 4d1 8c1 4- 4- 4c1 4c1 4c1 8e1 8g1 4- 4c1 4c1 4c1 8e1 8g1 4- 4c2 4d2 4c2 4#a1 4a1 4g1 8f1',
-        //         tempo: 225
-        //     },
-        //     simpsons: {
-        //         notes: '4c2 4e2 4#f2 8a2 4.g2 4e2 4c2 8a1 8#f1 8#f1 8#f1 2g1 4- 8#f1 8#f1 8#f1 8g1 4#a1 8c2 8c2 8c2 4c2',
-        //         tempo: 190
-        //     },
-        //     tiger: {
-        //         notes: '8d1 8e1 8f1 8- 8f1 16f1 8f1 16- 8e1 8d1 8c1 8c1 8d1 8e1 8d1 8- 8d1 8e1 8f1 16- 32- 8e1 8f1 8g1 16- 32- 8f1 16- 32a1 8- 16- 2a1 4- 8d1 16c1 8d1 16- 8c1',
-        //         tempo: 200
-        //     },
-        //     champions: {
-        //         notes: '2d2 8#c2 8d2 4#c2 4a1 8#f1 4b1 2#f1 8a1 2d2 8e2 8#f2 4a2 4#f2 16b1 2b1 4b1 4a1 8b1 4a1 4g1 4g2 4#f2 8g2 4#f2 4e2 4#f2 4d2 8g2 4#f2 4d2 8g2 4f2 4d2 8g2 4f2 2d2 8c2 8a1 ',
-        //         tempo: 100
-        //     }
-        // ]
-        // };
     }
-
-
-
-
-
-
     buttonClicked(num) {
         const buttonsSynth = new tone.Synth({
             frequency: 100,
@@ -421,11 +361,11 @@ export class HomePage {
         let counter = 0;
         document.getElementById('stop').click();
         let synth;
-        if (this.chorus === true) {
+        if (this.effectName === 'chorus') {
             const chorus = new tone.Chorus(4, 2.5, 0.5);
             synth = new tone.PolySynth(4, tone.MonoSynth).connect(chorus).toMaster();
         }
-        else if (this.distortion === true) {
+        else if (this.effectName === 'distortion') {
             const phaser = new tone.Phaser({
                 "frequency": 15,
                 "octaves": 5,
@@ -434,13 +374,13 @@ export class HomePage {
             synth = new tone.FMSynth().connect(phaser);
 
         }
-        else if (this.duosynth === true) {
+        else if (this.effectName === 'duosynth') {
             const reverb = new tone.JCReverb(0.4).connect(tone.Master);
             const delay = new tone.FeedbackDelay(0.5);
             //connecting the synth to reverb through delay
             synth = new tone.DuoSynth().chain(delay, reverb);
         }
-        else if (this.bitcrusher === true) {
+        else if (this.effectName === 'bitcrusher') {
             const crusher = new tone.BitCrusher(4).toMaster();
             synth = new tone.MonoSynth().connect(crusher);
         }
@@ -456,8 +396,7 @@ export class HomePage {
                     attackCurve: 'linear',
                     releaseCurve: 'step'
                 }
-                // harmonicity: 1.0,
-                // modulationIndex: 10
+
             }).toMaster();
         }
         const notesArray = this.inputBox.split(' ');
@@ -491,10 +430,6 @@ export class HomePage {
             }
         }, notesArray);
 
-        // var loop = new tone.Loop(function (time) {
-        //     //triggered every eighth note.
-        //     console.log(time);
-        // }, notesArray).start(0);
         this.seq.start();
         this.seq.loop = 0;
         tone.Transport.bpm.rampTo(this.value);
@@ -516,19 +451,10 @@ export class HomePage {
         this.inputBox = '';
 
         for (var i = 0; i < this.songs.length; i++) {
-            // console.log(
-            //     Object.keys(this.songs[i])[0]
-            //     );
 
             if(button.toLowerCase().includes(Object.keys(this.songs[i])[0]))
             {
                 console.log(Object.keys(this.songs[i])[0]);
-
-
-                //add here object name
-
-                // this.songs[i][Object.keys(this.songs[i])[0]].notes=this.inputBox;
-                // this.songs[i][Object.keys(this.songs[i])[0]].tempo=this.value;
 
                 const notes=this.songs[i][Object.keys(this.songs[i])[0]].notes;
                 const tempo=this.songs[i][Object.keys(this.songs[i])[0]].tempo;
@@ -541,30 +467,8 @@ export class HomePage {
                 tone.Transport.bpm.rampTo(this.value);
 
                 document.getElementById('play').click();
-                // const str=Object.keys(this.songs[i])[0];
-                // const name=Object.keys(this.songs[i][0]);
-                // console.log(this.songs[i][0]);
-
-
             }
         }
 
-        // for(const val of Object.keys(this.songs)){
-        //     console.log(this.songs[val]);
-        //     if (button.toLowerCase().includes(this.songs[val])) {
-        //         console.log(this.songs[val] + "found");
-        //     // tone.Transport.bpm.rampTo(this.songs[val].tempo);
-        //     // this.inputBox = this.songs[val].notes;
-        //     // this.value = this.songs[val].tempo + 50;
-        //     }
-        // for (const value of Object.keys(this.songs)) {
-        //     console.log(value);
-        //     if (button.toLowerCase().includes(value)) {
-        //         tone.Transport.bpm.rampTo(this.songs[value].tempo);
-        //         this.inputBox = this.songs[value].notes;
-        //         this.value = this.songs[value].tempo + 50;
-        //     }
-        // }
     }
-    // document.getElementById('play').click();
 }
